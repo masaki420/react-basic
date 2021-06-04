68044f0a42051760b54db4baeb1612a1e60e00be
masaki420 <zyab5275@gmail.com>
2021-06-04 09:58:39 +0900

    statewater

--- src/index.js
+++ src/index.js
@@ -4,18 +4,46 @@ import { render } from "react-dom";
 class Human extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {name: "Nakayama"};
+    this.state = {temp: 15};
+  }
+
+  H2OState(temp){
+    if(temp <= 0){
+      return "ice";
+    }
+    if(temp <= 100){
+      return "water";
+    }
+    if(temp => 100){
+      return "steam";
+    }
   }
 
   render() {
     return (
-    <h2 onClick={this.onButtonClick}>Hi {this.state.name} {this.props.age}</h2>
+      <>
+      <h2>phase: {this.H2OState(this.state.temp)}, {this.state.temp}</h2>
+
+      <button onClick={this.onPlusClick}>+</button>
+      <button onClick={this.onPlus10Click}>+10</button>
+      <button onClick={this.onMinusClick}>-</button>
+      <button onClick={this.onMinus10Click}>-10</button>
+      </>
     );
-    }
-  
-    onButtonClick= () => {
-      this.setState({name :"Aihara"});
-    }
+  }
+
+  onPlusClick=()=>{
+    this.setState({ temp : this.state.temp +1 });
+  };
+  onPlus10Click=()=>{
+    this.setState({ temp: this.state.temp + 10});
+  };
+  onMinusClick=()=>{
+    this.setState({ temp: this.state.temp -1 });
+  };
+  onMinus10Click=()=>{
+    this.setState({ temp: this.state.temp - 10});
+  };
 }
 
-render(<Human age="30"/>, document.getElementById("root"));
+render(<Human />, document.getElementById("root"));
