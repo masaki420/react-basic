7a18f1b6397a835964f5a6bb1824a3b8eafd6b65
masaki420 <zyab5275@gmail.com>
2021-06-04 11:30:43 +0900

    TodoApp

--- src/index.js
+++ src/index.js
@@ -1,56 +1,6 @@
+import { render } from "@testing-library/react";
 import React from "react";
-import { render } from "react-dom";
-import "./index.css";
+import { TodoApp } from "./TodoApp";
 
-class Human extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {temp: 15};
-  }
 
-  H2OState(temp){
-    if(temp <= 0){
-      return "ice";
-    }
-    if(temp <= 100){
-      return "water";
-    }
-    if(temp => 100){
-      return "steam";
-    }
-  }
-
-  render() {
-    const {temp} = this.state;
-
-    return (
-      <div className={this.H2OState(temp)}>
-      <h2>phase: {this.H2OState(temp)}, {temp}</h2>
-
-      <button onClick={this.onPlusClick}>+</button>
-      <button onClick={this.onPlus10Click}>+10</button>
-      <button onClick={this.onMinusClick}>-</button>
-      <button onClick={this.onMinus10Click}>-10</button>
-      </div>
-    );
-  }
-
-  onPlusClick=()=>{
-    const {temp} = this.state;
-    this.setState({ temp : temp +1 });
-  };
-  onPlus10Click=()=>{
-    const {temp} = this.state;
-    this.setState({ temp: temp + 10});
-  };
-  onMinusClick=()=>{
-    const {temp} = this.state;
-    this.setState({ temp: temp -1 });
-  };
-  onMinus10Click=()=>{
-    const {temp} = this.state;
-    this.setState({ temp: temp - 10});
-  };
-}
-
-render(<Human />, document.getElementById("root"));
+render(<TodoApp />, document.getElementById("root"));
